65fb6b8d60c6fa5188ccc211b84d481b49107731
irealwiz <irealwiz@tuta.io>
2024-06-13 19:34:27 +0800

    Добавляем конструктор алфавита

--- src/main/java/i2p/realmagic/cipher/caesar/Alphabet.java
+++ src/main/java/i2p/realmagic/cipher/caesar/Alphabet.java
@@ -11,5 +11,27 @@ public record Alphabet (
 	char alpha,
 	int size
 ) { // record body
+
+// static fields
+
+	/**
+	 * Максимально возможный размер алфавита.
+	 * <p>Совокупное число символов типа {@code char}.</p>
+	 */
+	public static final int MAX_ALPHABET_SIZE = 0x1_0000;
+
+// constructors
+
+	/**
+	 * Конструктор алфавита.
+	 * <p>Размер алфавита не должен превышать совокупное число символов типа {@code char}.</p>
+	 * @param alpha Первый символ алфавита.
+	 * @param size Размер алфавита.
+	 * @throws IllegalArgumentException Если указанный размер алфавита не является положительным числом, не превышающим общее число символов типа {@code char}.
+	 */
+	public Alphabet {
+		if ((size <= 0) || (size > MAX_ALPHABET_SIZE)) throw new IllegalArgumentException();
+	} // Alphabet()
+
 	// todo
 } // Alphabet

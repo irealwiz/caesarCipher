7a591939c0b6b665b59c3da6b31aec1d59fad20b
irealwiz <irealwiz@tuta.io>
2024-06-15 21:40:02 +0800

    Добавляем лицензию

--- src/main/java/i2p/realmagic/cipher/caesar/package-info.java
+++ src/main/java/i2p/realmagic/cipher/caesar/package-info.java
@@ -1,3 +1,15 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3 along with this code. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Sergey Shpagin
+ */
+
 /**
  * Пакет содержит классы, реализующие шифр Цезаря.
  */
